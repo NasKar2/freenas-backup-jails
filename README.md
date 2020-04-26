782fb6c3d8d8816137c773782f47a2ba6ffe402e
root <root@freenasSuper.local>
2020-04-25 20:48:10 -0400

    added option to keep multiple directories with date stamp containing backups

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ My pool is /mnt/v1 and iocage is located at /mnt/v1/iocage
 
 ## To Backup
 
-The script will backup all jails automatically if no other parameters are given
+he script will backup all jails automatically if no other parameters are given. The backups will be placed in a directory with the date in the name "IOCAGE__2020-04-25_1818".  You can specify in the script with the variable "maxNrOfBackups" how many backups to keep.
 
 ```
 ./jailbu.sh
@@ -28,14 +28,24 @@ will restore the plexpass jail from the backup files plexpass.zip and plexpass.s
 
 ### Prerequisites
 
-Edit these variables in the script and not the part in the ${}
+Edit these variables in the script and do not edit the part between the ${}
 
 POOL_NAME="/mnt/v1"
 
-logs_loc="${POOL_NAME}/backup/Jails/logs"
+LOGS_LOC="${POOL_NAME}/backup/Jails/logs" # In my pool example /mnt/v1/backup/Jails/logs
 
-FILE_NAME="Jail.log"
+FILE_NAME="JAIL.LOG"
 
-JAIL_DIR="${POOL_NAME}/iocage/jails"
+JAIL_DIR="${POOL_NAME}/iocage/jails" # /mnt/v1/iocage/jails
 
-BACKUP_LOC="${POOL_NAME}/backup/Jails"
+JAIL_IMAGE="${POOL_NAME}/iocage/images" # /mnt/v1/iocage/images
+
+BACKUP_LOC="${POOL_NAME}/backup/Jails" # /mnt/v1/backup/Jails
+
+FINAL="IOCAGE_"$(date +'_%F_%H%M') # IOCAGE__2020-04-25_2029
+
+BACKUP_DIR="${BACKUP_LOC}/${FINAL}" # The actual directory of the current backup - this is is subdirectory of the main directory above with a timestamp /mnt/v1/backup/Jails/IOCAGE__2020-04-25_2029
+
+FULL_LOG_NAME=${BACKUP_DIR}/$FILE_NAME # /mnt/v1/backup/Jails/IOCAGE__2020-04-25_2029/JAIL.LOG
+
+maxNrOfBackups=2 # The maximum number of backups to keep (when set to 0, all backups are kept)
--- jailbu.sh
+++ jailbu.sh
@@ -1,41 +1,80 @@
 #!/bin/bash
+# Variables
+cron=""
 POOL_NAME="/mnt/v1"
-logs_loc="${POOL_NAME}/backup/Jails/logs"
-FILE_NAME="Jail.log"
+LOGS_LOC="${POOL_NAME}/backup/Jails/logs"
+FILE_NAME="JAIL.LOG"
 JAIL_DIR="${POOL_NAME}/iocage/jails"
+JAIL_IMAGE="${POOL_NAME}/iocage/images"
 BACKUP_LOC="${POOL_NAME}/backup/Jails"
-FULL_LOG_NAME=${logs_loc}/${FILE_NAME}
-echo "FULL_LOG_NAME ="$FULL_LOG_NAME
-echo "logs_loc ="$logs_loc
+FINAL="IOCAGE_"$(date +'_%F_%H%M')
+BACKUP_DIR="${BACKUP_LOC}/${FINAL}" # The actual directory of the current backup - this is is subdirectory of the main directory above with a timestamp
+FULL_LOG_NAME=${BACKUP_DIR}/$FILE_NAME
+maxNrOfBackups=2 # The maximum number of backups to keep (when set to 0, all backups are kept)
+
+echo "LOGS_LOC = "${LOGS_LOC}
+echo "FULL_LOG_NAME = "$FULL_LOG_NAME
+echo "JAIL_IMAGE ="$JAIL_IMAGE
 echo "JAIL_DIR ="$JAIL_DIR
 echo "BACKUP_LOC ="$BACKUP_LOC
-if  [ $1 == "R" ] || [ $1 == "r" ]; then
-        iocage stop $2
-        iocage destroy $2
-        cp ${BACKUP_LOC}/${2}* ${POOL_NAME}/iocage/images/
-        iocage import $2
-        iocage start $2 
-        echo "Restore of "${2}" complete"
+echo "BACKUP_DIR = "$BACKUP_DIR
+
+if  [ "$1" = "R" ] || [ "$1" = "r" ]; then
+   #
+   # Pick the restore directory *don't edit this section*
+   #
+   cd $BACKUP_LOC
+   shopt -s dotglob
+   shopt -s nullglob
+   array=(*)
+     for dir in "${array[@]}"; do echo; done
+ 
+     for dir in */; do echo; done
+ 
+     echo "There are ${#array[@]} backups available pick the one to restore"; \
+     select dir in "${array[@]}"; do echo; break; done
+
+     echo "You choose ${dir}"
+
+
+     # More Variables
+     restore=$dir
+     currentRestoreDir="${BACKUP_LOC}/${restore}"
+     echo $currentRestoreDir
+     echo "$2 "$2
+
+           iocage stop $2
+           iocage destroy $2
+           cp ${currentRestoreDir}/${2}* ${JAIL_IMAGE}
+           iocage import $2
+           iocage start $2
+      echo "Restore of "${2}" from "${restore}" complete"
+
+
 else
-if [ ! -d "${BACKUP_LOC}" ]; then
-        mkdir -p "${BACKUP_LOC}"
+
+# echo "if BACKUP_LOC"
+if [ ! -d "$BACKUP_LOC" ]; then
         echo "create "${BACKUP_LOC}
-else
-        echo "The backup directory ${BACKUP_LOC} already exists!"
-        rm ${BACKUP_LOC}*
-        echo "Remove old Jail backups"
+        mkdir -p "${BACKUP_LOC}"
 fi
 
+#ls -l $BACKUP_LOC/"ioc*"
+echo "create logs"
 
-if [ ! -d "${logs_loc}" ]; then
-	mkdir -p "${logs_loc}"
-        echo "create "${logs_loc}
-else
-	echo "The backup directory ${logs_loc} already exists!"
-        rm ${FULL_LOG_NAME}
-        echo "Old log file removed"
-fi
-
+#if [ ! -d "${BACKUP_DIR}/logs" ]; then
+	mkdir -p "${BACKUP_DIR}"
+        echo "create "${BACKUP_DIR}
+#else
+#	echo "The backup directory ${LOGS_LOC} already exists!"
+#        rm ${FULL_LOG_NAME}
+#        echo "Old log file removed"
+#fi
+touch ${FULL_LOG_NAME}
+chmod 770 ${FULL_LOG_NAME}
+#echo "---------------------------"
+#echo "$(ls -l $BACKUP_DIR)"
+#echo "---------------------------"
 cd $JAIL_DIR
 shopt -s dotglob
 shopt -s nullglob
@@ -51,25 +90,55 @@ echo $(date) >> ${FULL_LOG_NAME}
 #Jail #1 Backup
 echo "Backing Up Jail" >> ${FULL_LOG_NAME}
 echo $(date) >> ${FULL_LOG_NAME}
+echo "Create file"${JAIL_DIR}"/images/"${JAIL_NAME}
+# touch ${JAIL_IMAGE}/${JAIL_NAME}
 iocage stop ${JAIL_NAME} >> ${FULL_LOG_NAME}
 iocage export ${JAIL_NAME} >> ${FULL_LOG_NAME}
 iocage start ${JAIL_NAME} >> ${FULL_LOG_NAME}
 
-if [ ! -d "${BACKUP_LOC}/${JAIL_NAME}*" ]; then
-        echo "No old "${JAIL_NAME}" backups in "${BACKUP_LOC}
-else
-        echo "Old "${JAIL_NAME}" backups exist in "${BACKUP_LOC}
-        echo "...Deleting old backups" >> ${FULL_LOG_NAME}
-        echo $(date) >> ${FULL_LOG_NAME}
-        rm ${BACKUP_LOC}/${JAIL_NAME}* >> ${FULL_LOG_NAME}
-fi
+#if [ ! -d "${BACKUP_DIR}/${JAIL_NAME}*" ]; then
+#        echo "No old "${JAIL_NAME}" backups in "${BACKUP_DIR}
+#else
+#        echo "Old "${JAIL_NAME}" backups exist in "${BACKUP_DIR}
+#        echo "...Deleting old backups" >> ${FULL_LOG_NAME}
+#        echo $(date) >> ${FULL_LOG_NAME}
+#        rm ${BACKUP_DIR}/${JAIL_NAME}* >> ${FULL_LOG_NAME}
+#fi
 
 echo "...Moving current backup to storage folders" >> ${FULL_LOG_NAME}
 echo $(date) >> ${FULL_LOG_NAME}
-mv -v ${POOL_NAME}/iocage/images/${JAIL_NAME}* ${BACKUP_LOC} >> ${FULL_LOG_NAME}
+mv -v ${JAIL_IMAGE}/${JAIL_NAME}* ${BACKUP_DIR} >> ${FULL_LOG_NAME}
 
 done
 
 echo "There are ${#array[@]} jails backed up"
+#fi
+
+#
+# Delete old backups
+#
+if [ ${maxNrOfBackups} -ne 0 ]
+then
+     echo "maxNrOfBackups is not 0"
+        nrOfBackups="$(ls -l ${BACKUP_LOC} | grep -c ^d)"
+     echo "nrOfBackups= "${nrOfBackups}
+        nDirToRemove="$((nrOfBackups - maxNrOfBackups))"
+
+     echo "nDirToRemove=" $nDirToRemove
+echo "****************"
+while [ $nDirToRemove -gt 0 ]
+do
+echo
+echo "number dir to remove=" $nDirToRemove
+dirToRemove="$(ls -t ${BACKUP_LOC} | tail -1)"
+echo "Removing Directory ${dirToRemove}"
+nDirToRemove="$((nDirToRemove - 1))"
+rm -r ${BACKUP_LOC}/${dirToRemove}
+done
 fi
 
+echo
+echo "DONE!"
+echo "Backup created: ${BACKUP_DIR}"
+#exit 1
+fi
